8c0685987a1782c386bb63f5e0ca8ed63287b591
Obie Ananda <Obie.kal22@gmail.com>
2025-03-05 22:04:47 +0700

    feat: added sumList exercise recursive implementation

---
+++ Exercise/SumList.py
@@ -0,0 +1,25 @@
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.next = None
+
+def sum_list(head):
+  if head is None:
+    return 0
+  return head.val + sum_list(head.next)
+
+if __name__ == "__main__":
+    a = Node(2)
+    b = Node(8)
+    c = Node(3)
+    d = Node(-1)
+    e = Node(7)
+
+    a.next = b
+    b.next = c
+    c.next = d
+    d.next = e
+
+    # 2 -> 8 -> 3 -> -1 -> 7
+
+    print(sum_list(a)) # 19
